696a6ec81bc8328962715185f2494866038e6450
rosie <rosie.bh.lee@gmail.com>
2023-06-08 15:07:34 -0700

    create scroll component

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ id, name, email }) => {
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <h1>Robo-Kitten-friends</h1>
+      <h2>Robo-😽-Friends</h2>
       <img src={`https://robohash.org/${id}?set=set4 `} alt="kittens" />
       <div>
         <h2>{name}</h2>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CardList from './CardList';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<CardList robots={robots} />);
+root.render(<App />);
 
 reportWebVitals();
